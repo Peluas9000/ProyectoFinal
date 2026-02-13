abc289054e9714e11fcfced0cdb2d37d37288da5
Ayoub <163521461+Peluas9000@users.noreply.github.com>
2026-02-13 08:31:44 +0100

    interfaces del inicio, registrar y login

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module benjaddi.ayoub.proyectofinal {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.base;
+
+    opens benjaddi.ayoub.proyectofinal to javafx.fxml;
+    exports benjaddi.ayoub.proyectofinal;
+}
